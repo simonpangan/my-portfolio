4945f282302679a3d10b4265cde5967853a1d369
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-08-04 22:19:06 +0800

    update favicon

--- Portfolio/.vitepress/config.ts
+++ Portfolio/.vitepress/config.ts
@@ -77,7 +77,52 @@ export default defineConfig({
           "MYSQL, SIMON PANGAN, FULL STACK DEVELOPER, FULL STACK, DEVELOPER"
       }
     ],
-    // ['li nk', { rel: 'icon', href: '/assets/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' }],
+    [
+      'link', {
+        rel: 'icon', 
+        type: "image/x-icon",
+        href: '/assets/favicon/favicon.ico',
+      }
+    ],
+    [
+      'link', {
+        rel: 'icon',
+        type: "image/png", 
+        sizes: "16x16",
+        href: '/assets/favicon/favicon-16x16.png',
+      }
+    ],
+    [
+      'link', {
+        rel: 'icon',
+        type: "image/png", 
+        sizes: "32x32",
+        href: '/assets/favicon/favicon-32x32.png',
+      }
+    ],
+    [
+      'link', {
+        rel: 'apple-touch-icon',
+        sizes: "180x180",
+        href: '/assets/favicon/apple-touch-icon.png',
+      }
+    ],
+    [
+      'link', {
+        rel: 'icon',
+        sizes: "512x512",
+        type: "image/png", 
+        href: '/assets/favicon/android-chrome-512x512.png',
+      }
+    ],
+    [
+      'link', {
+        rel: 'icon',
+        sizes: "192x192",
+        type: "image/png", 
+        href: '/assets/favicon/android-chrome-192x192.png',
+      }
+    ],
   ],
   ignoreDeadLinks: true,
   vite: {
