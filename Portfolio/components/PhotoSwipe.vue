4f124779a089af50ffb9885a77b2ee81a7d4d105
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-08-05 18:37:38 +0800

    update photoswipe

---
+++ Portfolio/components/PhotoSwipe.vue
@@ -0,0 +1,48 @@
+<script setup>
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
+import { onMounted, onUnmounted } from 'vue';
+
+
+const props = defineProps({
+	galleryID: String,
+	images: Array | Object
+})
+
+let lightbox;
+onMounted(() => {
+	lightbox = new PhotoSwipeLightbox({
+		gallery: '#' + props.galleryID,
+		children: 'a',
+		pswpModule: () => import('photoswipe'),
+	});
+	lightbox.init();
+})
+
+onUnmounted(() => {
+	if (lightbox) {
+		lightbox.destroy();
+		lightbox = null;
+	}
+})
+</script>
+
+<template>
+	<div :id="galleryID">
+		<div class="image-container">
+			<template v-if="Array.isArray(images)">
+				<a v-for="(image, key) in images" :key="key" :href="image.largeURL" :data-pswp-width="image.width"
+					:data-pswp-height="image.height" target="_blank" rel="noreferrer">
+					<img :src="image.thumbnailURL" :alt="image.alt" />
+				</a>
+			</template>
+			<template v-else>
+				<a :href="images.largeURL" :data-pswp-width="images.width"
+					:data-pswp-height="images.height" target="_blank" rel="noreferrer">
+					<img :src="images.thumbnailURL"  />
+				</a>
+			</template>
+		</div>
+	</div>
+</template>
+
